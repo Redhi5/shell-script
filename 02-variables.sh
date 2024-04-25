fe2a9643faccedb6a0386c4e38ea6a4eb5d48aa6
Redhi5 <redhi5.m@gmail.com>
2024-04-25 10:52:06 +0530

    First Commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,3 +4,9 @@ echo "Ramesh:: Hello Suresh, How are you?"
 echo "Suresh:: Hi Ramesh, I am fine. How are you?"
 echo "Ramesh: I am fine too. how is your work?"
 echo "Suresh:: not bad. I am thinking to upgrade to DevOps"
+
+echo "SRI-RAM:: Hello SEETHAAMMA, How are you?"
+echo "SEETHAAMMA:: Hi SRI-RAM, I am fine. How are you?"
+
+echo "SRI-RAM: I am fine too. how is your work?"
+echo "SEETHAAMMA:: not bad. I am thinking to upgrade to
